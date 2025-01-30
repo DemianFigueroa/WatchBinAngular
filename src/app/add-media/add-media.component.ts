ad229310ea657eca7e9efd6cfe29fae50851eab3
Demian Figueroa <demian.figueroa@hotmail.com>
2025-01-30 04:51:07 -0300

    main

--- src/app/add-media/add-media.component.ts
+++ src/app/add-media/add-media.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { AuthInterceptor } from '../auth.interceptor';
 import { ApiService } from '../api.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-add-media',
@@ -32,13 +33,13 @@ export class AddMediaComponent implements OnInit {
     description: '',
     category: '',
     status: false,
-    coverImageUrl: '',
+    coverImage: '', // Ensure this matches the property name in your template
   };
   errorMessage: string = '';
   successMessage: string = '';
   isEditing = false;
   mediaId: string | null = null;
-  defaultCoverImage = '/default-cover.jpg';
+  defaultCoverImage = '/assets/default-cover.jpg'; // Update the path to your default image
 
   constructor(
     private apiService: ApiService,
@@ -66,7 +67,7 @@ export class AddMediaComponent implements OnInit {
         this.media = {
           ...data,
           releaseDate: new Date(data.releaseDate).toISOString().split('T')[0],
-          coverImageUrl: data.coverImageUrl, // Use the image URL from the API
+          coverImage: data.coverImage, // Ensure this matches the property name in your template
         };
         this.cdr.detectChanges(); // Force change detection
       },
@@ -80,6 +81,8 @@ export class AddMediaComponent implements OnInit {
   onSubmit(): void {
     if (this.isEditing && this.mediaId) {
       this.media.id = this.mediaId;
+    }else{
+      this.media.id = uuidv4();
     }
 
     this.apiService.addMedia(this.media).subscribe(
@@ -93,5 +96,11 @@ export class AddMediaComponent implements OnInit {
         console.error(error);
       }
     );
+  }
+
+  // Handle image loading errors
+  onImageError(event: Event): void {
+    const imgElement = event.target as HTMLImageElement;
+    imgElement.src = this.defaultCoverImage; // Set the default image on error
   }
 }
